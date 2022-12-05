a513c96d2cc225d3e4e28df6d43e1babe485e4ec
hminsky2341 <harry.minsky@gmail.com>
2022-12-05 03:36:30 -0500

    trying out static serving using express as opposed to the whole serve nginx debauchery

--- backend/server.mjs
+++ backend/server.mjs
@@ -7,6 +7,11 @@ import './db.mjs';
 import {modifyProgramRoutes} from "./routes/modifyPrograms.mjs";
 import {hostRouter} from "./routes/hosts.mjs";
 import {getProgramRoutes} from "./routes/getPrograms.mjs";
+import * as path from "path";
+import {fileURLToPath} from "url";
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const server = express();
 
@@ -20,7 +25,12 @@ server.use('/api/modifyPrograms',modifyProgramRoutes);
 server.use('/api/getPrograms',getProgramRoutes);
 
 
+server.use(express.static(path.join(__dirname, "..","frontend", "build")));
+server.use(express.static("public"));
 
+server.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 mongoose.connect(`mongodb://${process.env.MONGO}/wnyu`).then(() => {
     console.log('mongoose connected!')
